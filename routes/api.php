94d7387ce7de7f775751b032c0caa7f319563854
nab1h <nabih_ahmed2563@yahoo.com>
2026-08-22 07:35:09 -0700

    done auth

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+
+// auth routes
+// ==========================
+Route::get('/user', function (Request $request) {return $request->user();})->middleware('auth:sanctum');
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
